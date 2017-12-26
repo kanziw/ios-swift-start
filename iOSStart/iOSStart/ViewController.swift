98d7d12f1ce8abdcf0089eb19f1d2e58e9e7bbd6
David Jung <david.jung@peeper.co>
2017-12-26 16:50:32 +0900

    버튼을 ViewController 에 Action 으로 연결하여 -/+ 기능 연결

--- iOSStart/iOSStart/ViewController.swift
+++ iOSStart/iOSStart/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        countLabel.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func addCount(value: Int) {
+        if let countString = countLabel.text, let count = Int(countString) {
+            countLabel.text = String(count + value)
+        }
+    }
 
-
+    @IBAction func plusAction(_ sender: UIButton) { addCount(value: 1) }
+    @IBAction func minusAction(_ sender: UIButton) { addCount(value: -1) }
 }
-
