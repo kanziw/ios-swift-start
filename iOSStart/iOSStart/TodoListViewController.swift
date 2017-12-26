763140239108314391de5b01fb4a5ab4dff01e83
David Jung <david.jung@peeper.co>
2017-12-26 18:35:49 +0900

    TableView 에 Sample data 입력
    1. extension 을 이용해 UITableViewDataSource protocol 연결
    2. TableViewCell 을 TableView 에 추가 -> identifier 를 Cell 로, Style 을 Basic 으로 줌
    3. 필수 함수 2개를 override
      - 섹션이 몇개인가
      - 셀엔 어떤 데이터가 보여져야 하는가?
    4. 임시로 ToDoList 탭을 첫 화면으로 옮김

--- iOSStart/iOSStart/TodoListViewController.swift
+++ iOSStart/iOSStart/TodoListViewController.swift
@@ -9,9 +9,27 @@
 import UIKit
 
 class TodoListViewController: UIViewController {
-
+    let sampleData = ["ABC","DEF","GHI"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "TO-DO LIST"
+        
+        // hide bottom line
+        tableView.tableFooterView = UIView()
+    }
+}
+
+extension TodoListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sampleData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = sampleData[indexPath.row]
+        return cell
     }
 }
