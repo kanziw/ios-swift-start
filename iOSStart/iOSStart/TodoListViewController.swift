5fc64596dd2bdfe0909e022769f81107239a8381
David Jung <david.jung@peeper.co>
2017-12-27 17:25:53 +0900

    textField 의 값을 Save 하여 리스트에 항목을 추가하는 기능 추가

--- iOSStart/iOSStart/TodoListViewController.swift
+++ iOSStart/iOSStart/TodoListViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class TodoListViewController: UIViewController {
-    let sampleData = ["ABC","DEF","GHI"]
+    var todoList = Array<String>()
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var stackViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,29 +26,40 @@ class TodoListViewController: UIViewController {
     }
     
     @objc func willShowKeyboard(_ noti: Notification) {
-//        print("Keyboard will show \(noti.userInfo)")
+        //        print("Keyboard will show \(noti.userInfo)")
         guard let userInfo = noti.userInfo, let duration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? Double, let keyboardFrame = userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue else {
             return
         }
         let height = keyboardFrame.cgRectValue.height
-
-//        let tabBarHeight = UITabBar().frame.height    // 0 으로 값이 나옴
+        
+        //        let tabBarHeight = UITabBar().frame.height    // 0 으로 값이 나옴
         let tabBarHeight = ((UIApplication.shared.delegate as? AppDelegate)?.window?.rootViewController as? UITabBarController)?.tabBar.frame.height ?? 0.0
         UIView.animate(withDuration: duration) {
             self.stackViewBottomConstraint.constant = self.stackViewBottomConstraint.constant + height - tabBarHeight
             self.view.setNeedsDisplay() // constranit 를 수정하면 내가 바뀌었다고 알려야 함
         }
     }
+    
+    @IBAction func saveButtonAction(_ sender: UIButton) {
+        guard let currentText = textField?.text else {
+            return
+        }
+        if (!currentText.isEmpty) {
+            todoList.append(currentText)
+            textField.text = ""
+            tableView.reloadData()
+        }
+    }
 }
 
 extension TodoListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sampleData.count
+        return todoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = sampleData[indexPath.row]
+        cell.textLabel?.text = todoList[indexPath.row]
         return cell
     }
 }
